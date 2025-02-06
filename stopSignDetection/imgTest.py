ea28886045a7e1545bbbfc72073a704dd9de5bd0
Ze'ev Krischer <zkri2558@uni.sydney.edu.au>
2025-02-06 14:57:38 +1100

    stop sign detection

--- stopSignDetection/imgTest.py
+++ stopSignDetection/imgTest.py
@@ -5,6 +5,27 @@ import glob
 def nothing(x):
     pass
 
+def countObjects(contours, image, draw=False):
+    largest_box = None
+    largest_area = 50  # Minimum area threshold
+
+    # Iterate through contours to find the one with the largest area
+    for contour in contours:
+        area = cv2.contourArea(contour)
+        if area > largest_area:
+            largest_area = area
+            x, y, w, h = cv2.boundingRect(contour)
+            largest_box = (x, y, w, h)
+
+    # If a bounding box is found and draw is True, create and show a copy of the image with the bounding rectangle
+    if largest_box and draw:
+        rectangleImage = image.copy()
+        x, y, w, h = largest_box
+        cv2.rectangle(rectangleImage, (x, y), (x + w, y + h), (255, 0, 0), 2)
+        return largest_box, rectangleImage
+
+    return largest_box, image  # Return the largest box or None, along with the original image
+
 
 # Load image
 
@@ -66,13 +87,12 @@ def img_test(image):
     cv2.destroyAllWindows()
     return 0
 
-def main():
-    images = glob.glob('img/*.png')   
-    print("Hello")
+def imges(images):
     result = []
     for image in images: 
         ses = cv2.imread(image)
         ses = cv2.resize(ses, (150,150))
+        ses = ses[100:,:]
         result.append(ses)
         
 
@@ -81,6 +101,42 @@ def main():
     
     
     img_test(img)
+
+
+def findStopSign(images):
+    hsv_lower = np.array([138, 125, 0])
+    hsv_upper = np.array([179,255,255])
+
+    for img in images:
+        image = cv2.imread(img)[115:,:]
+        
+        hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+        mask = cv2.inRange(hsv, hsv_lower, hsv_upper)
+        result = cv2.bitwise_and(image, image, mask=mask)
+
+        gray = cv2.cvtColor(result,cv2.COLOR_BGR2GRAY)
+        _,thresh = cv2.threshold(gray,1,255,cv2.THRESH_BINARY)
+        contours,hierarchy = cv2.findContours(thresh,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
+        contorImage = image.copy()
+        cv2.drawContours(contorImage, contours, -1, (0, 255, 0), 3)
+
+        stop, stop_image = countObjects(contours,image, True)
+
+        if stop != None:
+            return True
+
+        ims = cv2.hconcat([image,result,contorImage,stop_image])
+        cv2.imshow("test",ims)
+        cv2.waitKey(0)
+
+
+
+def main():
+    images = glob.glob('train_kat/*.jpg')   
+    findStopSign(images)
+    
+
+
 
 if __name__ == "__main__":
     main()
--- stopSignDetection/thresholdFinder.py
+++ stopSignDetection/thresholdFinder.py
@@ -4,6 +4,16 @@ import matplotlib.pyplot as plt
 from matplotlib.widgets import Slider
 from machinevisiontoolbox import Image, colorspace_convert
 
+def apply_hsv_threshold(img, hsv_values):
+    h_min, s_min, v_min, h_max, s_max, v_max = hsv_values
+    lower = np.array([h_min, s_min, v_min])
+    upper = np.array([h_max, s_max, v_max])
+    
+    hsv_image = colorspace_convert(img.image,'rgb','hsv')
+    mask = ((hsv_image >= lower) & (hsv_image <= upper)).all(axis=2)
+    result = hsv_image * mask[:, :, None]
+    return result
+
 def update(val, img, ax_img, fig, sliders):
     h_min = sliders['HMin'].val
     s_min = sliders['SMin'].val
@@ -42,8 +52,8 @@ def img_test(image):
     
     plt.show()
 
-def main():
-    images = glob.glob('img/*.png')
+def threshold_test():
+    images = glob.glob('img/*.jpg')
     result = []
     for image_path in images: 
         img = Image.Read(image_path)
@@ -55,7 +65,41 @@ def main():
         return
     
     img_concat = np.hstack(result)
-    img_test(img_concat)
+    img_test(img_concat[115:, :])
+
+
+def imges():
+    
+    h_min = 136.5
+    s_min = 0
+    v_min = 0
+    h_max = 255
+    s_max = 72.5
+    v_max = 85.5
+
+    hsv_values = [0,0,0,244,244,244]#[h_max, s_max, v_max, h_min, s_min, v_min]
+
+    images = glob.glob('img/*.jpg')
+
+    for image_path in images:
+        img = Image.Read(image_path)
+        img_resized = img[:,115:]
+        thresholded_img = apply_hsv_threshold(img_resized, hsv_values)
+        
+        fig, axs = plt.subplots(1, 2, figsize=(12, 6))
+        axs[0].imshow(img_resized.image)
+        axs[0].set_title('Original Image')
+        axs[0].axis('off')
+        
+        axs[1].imshow(thresholded_img)
+        axs[1].set_title('Thresholded Image')
+        axs[1].axis('off')
+        
+        plt.show()
+
+def main():
+    imges()
+   
 
 if __name__ == "__main__":
     main()
